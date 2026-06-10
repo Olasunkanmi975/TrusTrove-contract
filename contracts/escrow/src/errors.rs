4e7fcad27383c23d66460d8b57e9fa1b0504c901
K1NGD4VID <adesanyafuhad5@gmail.com>
2026-06-10 22:26:13 +0100

    feat(escrow): define EscrowError contracterror enum in errors.rs

---
+++ contracts/escrow/src/errors.rs
@@ -0,0 +1,11 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum EscrowError {
+    AlreadyInitialized = 1,
+    NotFound = 2,
+    NotAuthorized = 3,
+    AlreadyLocked = 4,
+    InvalidAmount = 5,
+}
